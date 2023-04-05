2071e19c821a4ea527a303fdd3937f1e21eabb7b
VndrGrhrd <vando124@gmail.com>
2023-04-05 08:48:18 -0300

    ejs

--- modulo-4/app.js
+++ modulo-4/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const routerPrincipal = require("./routes/principal");
 const routerPedidos = require("./routes/pedidos");
 
 const ports = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/", routerPrincipal);
 app.use("/pedidos", routerPedidos);
--- modulo-4/routes/pedidos.js
+++ modulo-4/routes/pedidos.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (_, res) => {
-  res.send("Pedido");
+  res.render("pedidos/index");
 });
 
 module.exports = router;
